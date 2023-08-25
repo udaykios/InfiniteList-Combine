66139b169279dcc8a930d2433c931153bf9b93d9
uday <uday@quintessential.design>
2023-08-25 14:37:39 +0530

    loader fixes

--- InfiniteList-Combine/Components/Loader.swift
+++ InfiniteList-Combine/Components/Loader.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct LoaderView: View {
     let isFailed: Bool
     var body: some View {
-        HStack{
+        HStack {
             ProgressView()
             Text(isFailed ? "Failed. Tap to retry." : "Loading...")
                 .foregroundColor(isFailed ? .red : .green)
